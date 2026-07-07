db1facd5b0ba02a6129cd31531774c207374bfad
Minyamir Kelemu <minyamirkelemu12@gmail.com>
2026-07-07 12:24:47 +0300

    Add imports for auth routes and user functions

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,3 +1,6 @@
+import express from 'express';
+import { registerUser, loginUser, getUserProfile } from '../controllers/auth.controller.js';
+import { protect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
