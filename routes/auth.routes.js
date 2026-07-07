b21a5bd1010ede35a7346a0d661d82a3c19aa6a8
Minyamir Kelemu <minyamirkelemu12@gmail.com>
2026-07-07 12:24:35 +0300

    Clean up imports in auth.routes.js
    Removed unused imports from auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,3 @@
-import express from 'express';
-import { registerUser, loginUser, getUserProfile } from '../controllers/auth.controller.js';
-import { protect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
