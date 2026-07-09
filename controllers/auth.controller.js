d21e4fe0e5f2fbca2ae8796b8c7a0f689c282618
Minyamir Kelemu <minyamirkelemu12@gmail.com>
2026-07-09 03:29:05 +0300

    Add token generation function for user authentication
    Added a function to generate JWT tokens for users.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
 
 // Register user
 export const registerUser = async (req, res) => {
