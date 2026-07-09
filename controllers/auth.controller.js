f4881e1613a20ead385daf16ad2ff1b4137f3fb1
Minyamir Kelemu <minyamirkelemu12@gmail.com>
2026-07-09 03:49:38 +0300

    Remove generateToken function
    Removed the generateToken function from auth.controller.js.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,6 @@
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-};
 
 // Register
 export const registerUser = async (req, res) => {
