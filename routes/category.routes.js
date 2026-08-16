21b9c00a52cfed06b9c4c495c3958a903464ef79
Minyamir Kelemu <minyamirkelemu12@gmail.com>
2026-08-16 14:00:21 +0300

    Update category.routes.js

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,12 +1,1 @@
-import express from 'express';
-import { getCategories, createCategory } from '../controllers/category.controller.js';
-import { protect } from '../middleware/auth.middleware.js';
-import { authorizeRoles } from '../middleware/role.middleware.js';
 
-
-const router = express.Router();
-
-router.get('/', getCategories);
-router.post('/', protect, authorizeRoles('admin'), createCategory);
-
-export default router;
