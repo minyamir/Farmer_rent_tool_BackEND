74c2776f78d50504619b19ba0b83193b4751dd83
Minyamir Kelemu <minyamirkelemu12@gmail.com>
2026-08-16 14:11:40 +0300

    Update machine.routes.js

--- routes/machine.routes.js
+++ routes/machine.routes.js
@@ -1,1 +1,23 @@
+import express from 'express';
+// --- ADD deleteMachine HERE ---
+import { 
+  createMachine, 
+  getMachines, 
+  getMachineById, 
+  deleteMachine 
+} from '../controllers/machine.controller.js'; 
+import { protect } from '../middleware/auth.middleware.js';
+import { authorizeRoles } from '../middleware/role.middleware.js';
 
+const router = express.Router();
+
+router.route('/')
+  .get(getMachines)
+  .post(protect, authorizeRoles('owner', 'admin'), createMachine);
+
+router.route('/:id')
+  .get(getMachineById)
+  // --- ADD THIS LINE ---
+  .delete(protect, authorizeRoles('owner', 'admin'), deleteMachine); 
+
+export default router;
